fdb23078201a8707d102b0ea98cedff82602ddb3
globo091 <virgilio.rizzi@edu.unifi.it>
2026-04-14 19:00:38 +0200

    Update ellens_alien_game.cpp

--- exercises/concept/ellens-alien-game/ellens_alien_game.cpp
+++ exercises/concept/ellens-alien-game/ellens_alien_game.cpp
@@ -1,4 +1,32 @@
 namespace targets {
-// TODO: Insert the code for the alien class here
+class Alien {
+private:
+    int health;
+public:
+    int x,y;
+    Alien(int x_coordinates, int y_coordinates, int health) : x(x_coordinates), y(y_coordinates), health(3) {}
+    int get_health() {
+        return health;
+    }
+    bool hit() {
+        health -= 1;
+        return true;
+    }
+    bool is_alive() {
+        if (health <= 0) {
+            return false;
+        }
+        return true;
+    }
+    bool teleport(int x_new, int y_new) {
+        return true;
+    }
+    bool collision_detection(Alien *alien, Alien *other) {
+        if (alien->x == other->x && alien->y == other->y) {
+            return true;
+        }
+        return false;
+    }
+};
+}
 
-}  // namespace targets
