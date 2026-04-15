327e8eb27a622011f40ffcca9b9063173e756bbe
globo091 <virgilio.rizzi@edu.unifi.it>
2026-04-15 17:43:23 +0200

    Update troll_the_trolls.cpp

--- exercises/concept/troll-the-trolls/troll_the_trolls.cpp
+++ exercises/concept/troll-the-trolls/troll_the_trolls.cpp
@@ -1,25 +1,52 @@
 namespace hellmath {
 
-// TODO: Task 1 - Define an `AccountStatus` enumeration to represent the four
-// account types: `troll`, `guest`, `user`, and `mod`.
+    // TODO: Task 1 - Define an `AccountStatus` enumeration to represent the four
+    // account types: `troll`, `guest`, `user`, and `mod`.
+    enum AccountStatus {
+        troll,
+        guest,
+        user,
+        mod,
+    };
 
-// TODO: Task 1 - Define an `Action` enumeration to represent the three
-// permission types: `read`, `write`, and `remove`.
+    // TODO: Task 1 - Define an `Action` enumeration to represent the three
+    // permission types: `read`, `write`, and `remove`.
+    enum Action {
+        read,
+        write,
+        remove,
+    };
 
-// TODO: Task 2 - Implement the `display_post` function, that gets two arguments
-// of `AccountStatus` and returns a `bool`. The first argument is the status of
-// the poster, the second one is the status of the viewer.
+    // TODO: Task 2 - Implement the `display_post` function, that gets two arguments
+    // of `AccountStatus` and returns a `bool`. The first argument is the status of
+    // the poster, the second one is the status of the viewer.
+    bool display_post(AccountStatus poster, AccountStatus wiever) {
+        return true;
+    }
+    // TODO: Task 3 - Implement the `permission_check` function, that takes an
+    // `Action` as a first argument and an `AccountStatus` to check against. It
+    // should return a `bool`.
+    bool permission_check(Action action, AccountStatus status) {
+        return true;
+    }
 
-// TODO: Task 3 - Implement the `permission_check` function, that takes an
-// `Action` as a first argument and an `AccountStatus` to check against. It
-// should return a `bool`.
+    // TODO: Task 4 - Implement the `valid_player_combination` function that
+    // checks if two players can join the same game. The function has two parameters
+    // of type `AccountStatus` and returns a `bool`.
+    bool valid_player_combination(AccountStatus status1, AccountStatus status2) {
+        if (status1==status2) {
+            return true;
+        }
+        return false;
+    }
 
-// TODO: Task 4 - Implement the `valid_player_combination` function that
-// checks if two players can join the same game. The function has two parameters
-// of type `AccountStatus` and returns a `bool`.
-
-// TODO: Task 5 - Implement the `has_priority` function that takes two
-// `AccountStatus` arguments and returns `true`, if and only if the first
-// account has a strictly higher priority than the second.
+    // TODO: Task 5 - Implement the `has_priority` function that takes two
+    // `AccountStatus` arguments and returns `true`, if and only if the first
+    // account has a strictly higher priority than the second.
+    bool has_priority(AccountStatus status1, AccountStatus status2) {
+        if (status1 > status2)
+            return true;
+        return false;
+    }
 
 }  // namespace hellmath
