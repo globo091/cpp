37eea08ed43cdcff9b399b1d40c971b419384b27
globo091 <virgilio.rizzi@edu.unifi.it>
2026-04-15 17:20:22 +0200

    Update election_day.cpp

--- exercises/concept/election-day/election_day.cpp
+++ exercises/concept/election-day/election_day.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 
-namespace election {
 
 // The election result struct is already created for you:
 
@@ -12,20 +11,25 @@ struct ElectionResult {
     int votes{};
 };
 
-// TODO: Task 1
-// vote_count takes a reference to an `ElectionResult` as an argument and will
-// return the number of votes in the `ElectionResult.
+int vote_count (ElectionResult &election_result) {
+    return election_result.votes;
+}
 
-// TODO: Task 2
-// increment_vote_count takes a reference to an `ElectionResult` as an argument
-// and a number of votes (int), and will increment the `ElectionResult` by that
-// number of votes.
+void increment_vote_count (ElectionResult &election_result, int vote_count) {
+    election_result.votes+=vote_count;
+}
 
-// TODO: Task 3
-// determine_result receives the reference to a final_count and returns a
-// reference to the `ElectionResult` of the new president. It also changes the
-// name of the winner by prefixing it with "President". The final count is given
-// in the form of a `reference` to `std::vector<ElectionResult>`, a vector with
-// `ElectionResults` of all the participating candidates.
+ElectionResult determine_result (std::vector<ElectionResult> &final_count) {
+    int max_votes = -1;
+    int winner_index = 0;
+    for (int i = 0; i < final_count.size(); i++) {
+        if (final_count[i].votes > max_votes) {
+            max_votes = final_count[i].votes;
+            winner_index = i;
+        }
+    }
+    final_count[winner_index].name = "President" + final_count[winner_index].name;
+    
+    return final_count[winner_index];
+}
 
-}  // namespace election
