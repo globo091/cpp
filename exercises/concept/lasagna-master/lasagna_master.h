3a3f7ae2b3cc8b8065eb594be381af42d7ea4149
globo091 <virgilio.rizzi@edu.unifi.it>
2026-04-21 14:01:40 +0200

    Update lasagna_master.h

--- exercises/concept/lasagna-master/lasagna_master.h
+++ exercises/concept/lasagna-master/lasagna_master.h
@@ -7,4 +7,15 @@ struct amount {
     double sauce;
 };
 
+    int preparationTime(std::vector<std::string>& layers, int avg_time=2) {}
+
+    amount quantities(const std::vector<std::string>& layers) {}
+
+    void addSecretIngredient(std::vector<std::string>& myList,
+        const std::vector<std::string>& friendsList) {}
+
+    std::vector<double> scaleRecipe(const std::vector<double>& quantities, int portions) {}
+
+    void addSecretIngredient(std::vector<std::string>& myList, const std::string& secretIngredient) {}
+
 }  // namespace lasagna_master
